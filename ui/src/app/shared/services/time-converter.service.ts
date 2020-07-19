de95005f9f419e391060c9e828a06d3e63970558
MyCah Pleasant <mycahp@gmail.com>
2020-07-19 05:12:06 +0000

    Styling fixes

--- ui/src/app/shared/services/time-converter.service.ts
+++ ui/src/app/shared/services/time-converter.service.ts
@@ -9,7 +9,7 @@ export class TimeConverterService {
 
   getVideoLength(videoLength: number): string {
     const minutes: number = Math.floor(videoLength / 60);
-    const seconds = videoLength - (minutes * 60);
+    const seconds = Math.round(videoLength - (minutes * 60));
 
     return `${minutes}:${String(seconds).padStart(2, '0')}`;
   }
